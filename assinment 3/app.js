d46756e2241313d74a757a827b72ec5632935529
Abdul Ahad <121622598+AbdulAhad7692@users.noreply.github.com>
2023-02-05 21:01:01 +0500

    Add files via upload

--- assinment 3/app.js
+++ assinment 3/app.js
@@ -181,7 +181,7 @@ var radius = 20;
 var circumfrance;
 circumfrance = 2 * 3.142 * 20;
 var area;
-area = 3.142 * (20*20);
+area = 3.142 * (20 * 20);
 document.write("The radius of the circle is : " + radius)
 document.write("<br>")
 document.write("The circumfrance of the circle is : " + circumfrance)
@@ -192,11 +192,11 @@ document.write("The area of circle is : " + area)
 
 document.write("<h1>The lifetime supply calculator</h1>")
 
-var chai="chai";
+var chai = "chai";
 var CurrentAge = 18;
 var MaxAge = 65;
 var max_snack = 3;
- 
+
 document.write("Your Favourite Snack : " + chai)
 document.write("<br>")
 document.write("Your Current Age : " + CurrentAge)
@@ -205,12 +205,146 @@ document.write("Estimated Maximum Age : " + MaxAge)
 document.write("<br>")
 document.write("Amount of Snacks Per Day : " + max_snack)
 document.write("<br>")
-var total_snack =((MaxAge - CurrentAge) * (365 * max_snack))
-document.write("You will need " + total_snack +" to last you until the ripe old age of 65")
+var total_snack = ((MaxAge - CurrentAge) * (365 * max_snack))
+document.write("You will need " + total_snack + " to last you until the ripe old age of 65")
+document.write("<br>")
+
+// CHAPTER 6 //
+
+document.write("<h1>CHAPTER 6-9</h1>")
+
+// QUESTION NO 1 //
+
+document.write("Result : ")
+document.write("<br>")
+document.write("The Value of a is : 10")
+document.write("<br>")
+document.write("_________________________")
+document.write("<br>")
+document.write("<br>")
+
+var a = 10;
+a++
+document.write("The value of ++a is : " + a);
+document.write("<br>")
+document.write("Now The Value of  a is : " + a)
+document.write("<br>")
+document.write("<br>")
+document.write("The Value of a++ is : " + a)
+document.write("<br>")
+a++
+document.write("Now the Value of a is : " + a)
+document.write("<br>")
+document.write("<br>")
+
+--a 
+document.write("The value of --a is : " + a)
+document.write("<br>")
+document.write("Now the value of a is : " + a)
+document.write("<br>")
+document.write("<br>")
+document.write("The value of a-- is : " + a)
+a--
+document.write("<br>")
+document.write("Now the value of a is : " + a)
+document.write("<br>")
+document.write("<br>")
 document.write("<br>")
 
 
+// QUESTION NO 2 //
+var b = 2;
+var c = 1;
+document.write("The Value of a is  " + b)
+document.write("<br>")
+document.write("The Value of b is " + c)
+document.write("<br>")
+document.write("<br>")
 
+--b;
+--b - --c;
+--b - --c + ++c;
+--b - --c + ++c + c--;
+document.write("Result is : " + b)
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+// QUESTION NO 3 //
+var data = prompt("Enter Your Name"); 
+alert("Welcome "  + data);
+document.write("<br>")
+document.write("<br>")
+
+// QUESTION NO 5 //
+var t_data = prompt("Enter Number I'll show you it's table " , "5")
+document.write( t_data + " x 1 = " + t_data * 1)
+document.write("<br>")
+document.write( t_data + " x 2 = " + t_data * 2)
+document.write("<br>")
+document.write( t_data + " x 3 = " + t_data * 3)
+document.write("<br>")
+document.write( t_data + " x 4 = " + t_data * 4)
+document.write("<br>")
+document.write( t_data + " x 5 = " + t_data * 5)
+document.write("<br>")
+document.write( t_data + " x 6 = " + t_data * 6)
+document.write("<br>")
+document.write( t_data + " x 7 = " + t_data * 7)
+document.write("<br>")
+document.write( t_data + " x 8 = " + t_data * 8)
+document.write("<br>")
+document.write( t_data + " x 9 = " + t_data * 9)
+document.write("<br>")
+document.write( t_data +" x 10 = " + t_data * 10)
+document.write("<br>")
+document.write("<br>")
+
+//QUESTION NO 6 //
+
+var sub1 = prompt("Subject 1","English")
+var sub2 = prompt("Subject 2","Urdu")
+var sub3 = prompt("Subject 3","Maths")
+var t_marks = 100 ;
+
+sub1marks = prompt("Enter " + sub1 + " Marks ")
+sub2marks = prompt("Enter " + sub2 + " Marks ")
+sub3marks = prompt("Enter " + sub3 + " Marks ")
+document.write("<br>")
+var sub1marks;
+var sub2marks;
+var sub3marks;
+
+document.write("<h4>Subject 1</h4>")
+document.write("Subject : "+ sub1) 
+document.write("<br>")
+document.write("Total marks is : " + t_marks)
+document.write("<br>")
+document.write("Obtained Marks is : " + sub1marks)
+
+document.write("<h4>Subject 2</h4>")
+document.write("Subject : "+ sub2) 
+document.write("<br>")
+document.write("Total marks is : " + t_marks)
+document.write("<br>")
+document.write("Obtained Marks is : " + sub2marks)
+
+document.write("<h4>Subject 3</h4>")
+document.write("Subject : "+ sub3) 
+document.write("<br>")
+document.write("Total marks is : " + t_marks)
+document.write("<br>")
+document.write("Obtained Marks is : " + sub3marks)
+
+document.write("<h2>Percentage</h2>")
+var result = -(-sub1marks)-(-sub2marks)-(-sub3marks)
+document.write("Total Marks Obtained is : " + result)
+document.write("<br>")
+document.write("Total Marks : " + t_marks*3 )
+document.write("<br>")
+var per;
+per = (result*100) / 300;
+document.write("Your Percentage is :  " + per)
 
 
 
